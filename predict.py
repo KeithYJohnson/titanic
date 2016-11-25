02740f3a547d2631e24a3e1121deb4021dd32e96
Keith Johnson <keithyjohnson@gmail.com>
2016-11-25 16:23:33 -0600

    Impute data for "Age" by using the median and "Embarked" by most frequent value.

--- predict.py
+++ predict.py
@@ -7,6 +7,11 @@ NUMBER_OF_CLASSIFIERS = 2
 
 training_data = pd.DataFrame.from_csv('train.csv')
 theta1 = rand_initialize_weights(training_data.shape[0], NUMBER_OF_HIDDEN_UNITS)
+# Impute missing data
+training_data["Age"] = training_data["Age"].fillna(training_data["Age"].median())
+training_data["Embarked"] = training_data["Embarked"].fillna(
+  training_data['Embarked'].value_counts().idxmax()
+)
 theta2 = rand_initialize_weights(NUMBER_OF_HIDDEN_UNITS, NUMBER_OF_CLASSIFIERS)
 
 compute_cost(training_data, theta1, theta2)
