b030d18994e116ec21f4501f830a3936a63bf2d8
Keith Johnson <keith@zipmark.com>
2016-11-25 16:27:26 -0600

    oops forgot to stage deletions.

--- predict.py
+++ predict.py
@@ -2,11 +2,8 @@ import pandas as pd
 from compute_cost import compute_cost
 from rand_initialize_weights import rand_initialize_weights
 
-NUMBER_OF_HIDDEN_UNITS = 25
-NUMBER_OF_CLASSIFIERS = 2
 
 training_data = pd.DataFrame.from_csv('train.csv')
-theta1 = rand_initialize_weights(training_data.shape[0], NUMBER_OF_HIDDEN_UNITS)
 # Impute missing data
 training_data["Age"] = training_data["Age"].fillna(training_data["Age"].median())
 training_data["Embarked"] = training_data["Embarked"].fillna(
@@ -35,5 +32,4 @@ NUMBER_OF_CLASSIFIERS = 2   # It's binary, either they survived or didn't
 theta1 = rand_initialize_weights(INPUT_LAYER_SIZE, NUMBER_OF_HIDDEN_UNITS)
 theta2 = rand_initialize_weights(NUMBER_OF_HIDDEN_UNITS, NUMBER_OF_CLASSIFIERS)
 
-compute_cost(training_data, theta1, theta2)
 compute_cost(features, theta1, theta2)
