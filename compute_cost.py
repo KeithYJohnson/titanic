8996ef004f1db45ac7a51e6a4698065f053c3882
Keith Johnson <keithyjohnson@gmail.com>
2016-11-30 15:47:00 -0500

    Splitting cost calculation and gradient computing into their own functions.

--- compute_cost.py
+++ compute_cost.py
@@ -21,44 +21,4 @@ def compute_cost(weights, features, y):
     num_examples = y.shape[0]
     # Vectorized Cost
     cost = (1/num_examples) * np.sum(y_eq_1_term - y_eq_0_term)
-
-    #Accumulators used to calculate the partial derivative of J(Theta)
-    w3_partial_deriv_wrt_j = np.zeros(w3.shape)
-    w2_partial_deriv_wrt_j = np.zeros(w2.shape)
-
-    #Iterative backprop, TODO vectorize
-    for row_num in range(0, a1.shape[0]):
-        #Get row, prepend a 1 as the bias, and then reshape into 8d vector
-        num_rows = features.shape[1]
-        a1 = np.insert(features[row_num, :], 0, 1, 0).reshape(num_rows + 1,1)
-        z2 = np.dot(w2, a1)
-        a2 = sigmoid(z2)
-        a2 = np.insert(a2, 0, 1, 0)
-        z3 = np.dot(w3, a2)
-        a3 = sigmoid(z3)
-
-        #Error of the neurons in layer 3
-        d3 = a3 - y[row_num, :] # Diff bt prediction and actual outcome for the row.
-
-        a3_deriv = sigmoid_gradient(z3)
-        # Error of neurons in layer two
-        d2 = np.multiply(
-            np.dot(w3.transpose(), d3),
-            a3_deriv
-        )
-
-        w3_partial_deriv_wrt_j =  np.add(
-            w3_partial_deriv_wrt_j,
-            np.dot(d3, a2.transpose())
-        )
-
-        w2_partial_deriv_wrt_j = np.add(
-            w2_partial_deriv_wrt_j,
-                   #Dont care about change in bias
-            np.dot(d2[1:d2.shape[0],:], a1.transpose())
-        )
-
-    w2_grad = (1/num_examples) * w2_partial_deriv_wrt_j
-    w3_grad = (1/num_examples) * w3_partial_deriv_wrt_j
-
-    return cost, w2_grad, w3_grad
+    return cost
