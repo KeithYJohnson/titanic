Fixing some bugs and rewriting regularized cost computation to be a bit more readable

compute_cost.py
@@ -18,7 +18,6 @@ def compute_cost(weights,
     w2 = weights[0:w2w3_idx_boundary].reshape(
         hidden_units, input_size + 1
     )
-
     w3 = weights[w2w3_idx_boundary:].reshape(
         output_size, hidden_units + 1
     )
@@ -29,14 +28,18 @@ def compute_cost(weights,
     a2 = add_bias_column(a2)
     z3 = np.dot(a2, w3.transpose())
     a3 = sigmoid(z3)
-    z3 = np.dot(a2, w3.transpose())
-    a3 = sigmoid(z3) # Predictions
 
-    y_eq_1_term = np.multiply(y, np.log(a3))
-    y_eq_0_term = np.multiply((1 - y), np.log(1 - a3))
     num_examples = y.shape[0]
-    # Vectorized Cost
-    cost = (1/num_examples) * np.sum(y_eq_1_term - y_eq_0_term) \
-           + (regularization_strength / (2 * num_examples)) * sum(weights[1:] ** 2) #Regularization Term. 
+
+    y_eq_1_term = np.multiply(-y, np.log(a3))
+    y_eq_0_term = np.multiply((1 - y), np.log(1 - a3))
+    unregularized_cost = (1/num_examples) * np.sum(y_eq_1_term - y_eq_0_term)
+
+    w2_regulation_term = np.sum(w2[:,2:] ** 2);
+    w3_regulation_term = np.sum(w3[:,2:] ** 2);
+    regularization     = (regularization_strength / (2 * num_examples)) * (w2_regulation_term + w3_regulation_term)
+
+    cost = unregularized_cost + regularization
+
     print(cost)
     return cost