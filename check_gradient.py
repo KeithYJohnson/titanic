cd076cf0276f98eae6457abbc72a0d4e2087bd07
Keith Johnson <keithyjohnson@gmail.com>
2016-12-04 13:41:39 -0500

    renaming some vars in check_gradient

--- check_gradient.py
+++ check_gradient.py
@@ -5,24 +5,24 @@ import numpy as np
 def check_gradient(cost_function, grad_function, epsilon = 10 ** -4, max_diff=1e-9):
     print('checking gradients')
     [features, theta1, theta2, y] = create_simple_nn_params()
-    unrolled_weights = np.hstack([theta1.flatten(), theta2.flatten()])
-    cost = cost_function(unrolled_weights, features, y, features.shape[1], theta1.shape[0], 1)
-    grads = grad_function(unrolled_weights, features, y, features.shape[1], theta1.shape[0], 1)
+    rolled_weights = np.hstack([theta1.flatten(), theta2.flatten()])
+    cost = cost_function(rolled_weights, features, y, features.shape[1], theta1.shape[0], 1)
+    grads = grad_function(rolled_weights, features, y, features.shape[1], theta1.shape[0], 1)
 
-    unrolled_weights_approx = np.zeros(unrolled_weights.shape)
-    for index, x in np.ndenumerate(unrolled_weights):
-        unrolled_weights_plus = unrolled_weights
-        unrolled_weights_plus[index] += epsilon
-        Jplus = cost_function(unrolled_weights_plus, features, y, features.shape[1], theta1.shape[0], 1)
+    rolled_weights = np.zeros(rolled_weights.shape)
+    for index, x in np.ndenumerate(rolled_weights):
+        rolled_weights_plus = rolled_weights
+        rolled_weights_plus[index] += epsilon
+        Jplus = cost_function(rolled_weights_plus, features, y, features.shape[1], theta1.shape[0], 1)
 
-        unrolled_weights_minus = unrolled_weights
-        unrolled_weights_minus[index] -= epsilon
-        Jminus = cost_function(unrolled_weights_minus, features, y, features.shape[1], theta1.shape[0], 1)
+        rolled_weights_minus = rolled_weights
+        rolled_weights_minus[index] -= epsilon
+        Jminus = cost_function(rolled_weights_minus, features, y, features.shape[1], theta1.shape[0], 1)
 
         numerical_grad = (Jplus - Jminus) / (2 * epsilon)
-        unrolled_weights_approx[index] = numerical_grad
+        rolled_weights[index] = numerical_grad
 
     print('abs element-wise diff between numerical grad against analytical grad')
-    print(np.abs(unrolled_weights_approx - grads))
+    print(np.abs(rolled_weights - grads))
     print('element-wise: is the diff larger than max_diff of: ', max_diff)
-    print(np.abs(unrolled_weights_approx - grads) > max_diff)
+    print(np.abs(rolled_weights - grads) > max_diff)
