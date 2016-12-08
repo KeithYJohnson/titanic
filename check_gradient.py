7e55d621a950b3515c7f18234db050113d924cac
Keith Johnson <keithyjohnson@gmail.com>
2016-12-08 11:39:38 -0500

    Adding relative_error_diff to check_gradient just for curiousity's sake. - Also renaming vars to analytical and numerical grad - Splitting off different ways of calculating grads into functions.

--- check_gradient.py
+++ check_gradient.py
@@ -9,21 +9,34 @@ def check_gradient(cost_function, grad_function, epsilon = 10 ** -4, max_diff=1e
     cost = cost_function(rolled_weights, features, y, input_layer_size, hidden_layer_size, output_layer_size, 4)
     analytical_grad = grad_function(rolled_weights, features, y, input_layer_size, hidden_layer_size, output_layer_size, 4)
 
-    rolled_weights_appox = np.zeros(rolled_weights.shape)
+    numerical_grad = np.zeros(rolled_weights.shape)
     perturb = np.zeros(rolled_weights.shape)
-    for index, x in np.ndenumerate(rolled_weights_appox):
+    for index, x in np.ndenumerate(numerical_grad):
         perturb[index] = epsilon
 
         Jplus = cost_function((rolled_weights + perturb), features, y, input_layer_size, hidden_layer_size, output_layer_size)
         Jminus = cost_function((rolled_weights - perturb), features, y, input_layer_size, hidden_layer_size, output_layer_size)
 
-        numerical_grad = (Jplus - Jminus) / (2 * epsilon)
-        rolled_weights_appox[index] = numerical_grad
+        weight_grad = (Jplus - Jminus) / (2 * epsilon)
+        numerical_grad[index] = weight_grad
         perturb[index] = 0
 
     print('abs element-wise diff between numerical grad against analytical grad')
-    print(np.abs(rolled_weights_appox - grads))
-    print('\nnp.linalg.norm(rolled_weights - grads)/np.linalg.norm(rolled_weights + grads);')
+    print(np.abs(numerical_grad - analytical_grad))
+    relative_error_diff(numerical_grad, analytical_grad)
+    norm_diff(numerical_grad, analytical_grad, max_diff)
+
+
+
+
+
+## SEE http://cs231n.github.io/neural-networks-3/
+def relative_error_diff(numerical_grad, analytical_grad):
+    diff = abs(analytical_grad - numerical_grad) / np.maximum(abs(numerical_grad), abs(analytical_grad))
+    print("relative_error_diff: ", diff)
+
+def norm_diff(numerical_grad, analytical_grad, max_diff):
+    print('\nnp.linalg.norm(rolled_weights - analytical_grad)/np.linalg.norm(rolled_weights + analytical_grad);')
     print('should be less than the max_diff of: ', max_diff)
-    diff = np.linalg.norm(rolled_weights_appox - grads)/np.linalg.norm(rolled_weights_appox + grads);
+    diff = np.linalg.norm(numerical_grad - analytical_grad) / np.linalg.norm(numerical_grad + analytical_grad);
     print('diff: ', diff, '\n\n')
